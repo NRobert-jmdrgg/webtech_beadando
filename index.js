82b4cfb510fa17f8c2bc08250d48b631d4c9a5b2
Róbert Nagy <rob.iskola1@gmail.com>
2022-10-26 18:18:31 +0200

    footer, dark mode icon, hover

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ $('.dark-mode-switcher-container').click(() => {
       '--main-text-color': 'hsl(228, 56%, 86%)',
       '--main-border-color': 'hsl(228, 36%, 79%)',
     });
+    $('#dark-mode-switcher').attr('src', './resources/dark_mode.png');
     $('#logo').attr('src', './resources/logo_dark.png');
   } else {
     $(':root').css({
@@ -14,22 +15,23 @@ $('.dark-mode-switcher-container').click(() => {
       '--main-text-color': 'hsl(234, 16%, 35%)',
       '--main-border-color': 'hsl(233, 13%, 41%)',
     });
+    $('#dark-mode-switcher').attr('src', './resources/light_mode.png');
     $('#logo').attr('src', './resources/logo.png');
   }
 });
 
-let before = true;
 fetch('https://baconipsum.com/api/?type=all-meat&paras=15&start-with-lorem=1')
   .then((response) => response.json())
   .then((data) => {
     data.forEach((sentence, i) => {
-      before = !before;
+      before = i % 2 === 0;
       const id = `#bi${i}`;
       const img =
         '<img src="" alt="dog image" style="width: 100px; height: 100px;"></img>';
       $('.list').append(
         `<div class='bacon-ipsum' id='bi${i}'><span>${sentence}</span></div>`
       );
+
       before ? $(id).prepend(img) : $(id).append(img);
       fetch('https://dog.ceo/api/breeds/image/random')
         .then((response) => response.json())

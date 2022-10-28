1314cd4c8310f14eaf5f65d5a9f4ce2bcd2fcdbc
Róbert Nagy <rob.iskola1@gmail.com>
2022-10-28 16:58:44 +0200

    receptek, dark mode fix, menu animációk

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-$('.dark-mode-switcher-container').click(() => {
-  if ($('body').css('background-color') === 'rgb(221, 225, 233)') {
-    $(':root').css({
-      '--main-bg-color': 'hsl(229, 20%, 17%)',
-      '--navbar-color': 'hsl(231, 19%, 20%)',
-      '--main-text-color': 'hsl(228, 56%, 86%)',
-      '--main-border-color': 'hsl(228, 36%, 79%)',
-    });
-    $('#dark-mode-switcher').attr('src', './resources/dark_mode.png');
-    $('#logo').attr('src', './resources/logo_dark.png');
+function setDarkThemeImages() {
+  $('#dark-mode-switcher').attr('src', './resources/dark_mode.png');
+  $('#logo').attr('src', './resources/logo_dark.png');
+}
+
+function setLightThemeImages() {
+  $('#dark-mode-switcher').attr('src', './resources/light_mode.png');
+  $('#logo').attr('src', './resources/logo.png');
+}
+
+$(document).ready(function () {
+  const theme = localStorage.theme;
+
+  // logo fix
+  if (theme === 'dark') {
+    setDarkThemeImages();
   } else {
-    $(':root').css({
-      '--main-bg-color': 'hsl(220, 21%, 89%)',
-      '--navbar-color': 'hsl (220, 22%, 92%)',
-      '--main-text-color': 'hsl(234, 16%, 35%)',
-      '--main-border-color': 'hsl(233, 13%, 41%)',
-    });
-    $('#dark-mode-switcher').attr('src', './resources/light_mode.png');
-    $('#logo').attr('src', './resources/logo.png');
+    setLightThemeImages();
   }
 });
 
-$('.dropdown').hover(
-  function () {
-    $(this).show();
-  },
-  function () {
-    $(this).hide();
+$('.dark-mode-switcher-container').click(() => {
+  const theme = localStorage.getItem('theme');
+  if (theme === 'light') {
+    setDarkTheme();
+    setDarkThemeImages();
+  } else {
+    setLightTheme();
+    setLightThemeImages();
   }
-);
+});
 
-$('.navbar-toggle').hover(
-  function () {
-    const id = $(this).attr('id');
-    $('.dropdown').not(`#${id}`).hide();
+// dropdown menu animations
+$(function () {
+  $('.dropdown').hover(
+    function () {
+      $(this).show();
+    },
+    function () {
+      $(this).slideUp(200);
+    }
+  );
 
-    $(`#${id}.dropdown`).show();
-  },
-  function () {
-    const id = $(this).attr('id');
-    $(`#${id}.dropdown`).hide();
-  }
-);
+  $('.navbar-toggle').hover(
+    function () {
+      const id = $(this).attr('id');
+      $('.dropdown').not(`#${id}`).slideUp(200);
+
+      $(`#${id}.dropdown`).slideDown(200);
+    },
+    function () {
+      const id = $(this).attr('id');
+      $(`#${id}.dropdown`).hide();
+    }
+  );
+});
---
+++ setTheme.js
@@ -0,0 +1,38 @@
+function setDarkTheme() {
+  localStorage.setItem('theme', 'dark');
+  // switch to dark theme
+  $(':root').css({
+    '--main-bg-color': 'hsl(229, 20%, 17%)',
+    '--secondary-bg-color': 'hsl(229, 19%, 23%)',
+    '--navbar-color': 'hsl(231, 19%, 20%)',
+    '--main-text-color': 'hsl(228, 56%, 86%)',
+    '--main-border-color': 'hsl(228, 36%, 79%)',
+  });
+}
+
+function setLightTheme() {
+  localStorage.setItem('theme', 'light');
+  $(':root').css({
+    '--main-bg-color': 'hsl(220, 21%, 89%)',
+    '--secondary-bg-color': 'hsl(220, 23%, 95%)',
+    '--navbar-color': 'hsl (220, 22%, 92%)',
+    '--main-text-color': 'hsl(234, 16%, 35%)',
+    '--main-border-color': 'hsl(233, 13%, 41%)',
+  });
+}
+
+// set theme
+let theme = localStorage.getItem('theme');
+if (!theme) {
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    console.log('hello dark');
+    theme = 'dark';
+  } else {
+    console.log('hello light');
+    theme = 'light';
+  }
+}
+theme === 'light' ? setLightTheme() : setDarkTheme();

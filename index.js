b521f6194f36ac5f8e87110202ec308f93ea37f9
Róbert Nagy <rob.iskola1@gmail.com>
2022-10-29 22:54:52 +0200

    breakfast image

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function setDarkThemeImages() {
   $('#dark-mode-switcher').attr('src', './resources/dark_mode.png');
   $('#logo').attr('src', './resources/logo_dark.png');
   $('.requirements').css('list-style', 'url("./resources/tick_dark.png")');
+  $('#breakfast-logo').attr('src', './resources/logo-breakfast-dark.png');
 }
 
 function setLightThemeImages() {
   $('#dark-mode-switcher').attr('src', './resources/light_mode.png');
   $('#logo').attr('src', './resources/logo.png');
   $('.requirements').css('list-style', 'url("./resources/tick.png")');
+  $('#breakfast-logo').attr('src', './resources/logo-breakfast.png');
 }
 
 $(document).ready(function () {
